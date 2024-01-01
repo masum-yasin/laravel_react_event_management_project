2d808b61e88cab4b61d55dc13420f2f2e525c159
masum-yasin <masum55549@gmail.com>
2024-01-01 15:33:23 +0600

    first commit

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+    DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/masum-yasin" target="_blank">github-link By Md Mashum Hossain</a>
+</div>
---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Manup Template">
+    <meta name="keywords" content="Manup, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Manup | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap" rel="stylesheet">
+
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="frontend/assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/assets/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/assets/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="frontend/assets/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/assets/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="frontend/assets/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/assets/css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+   @include('frontend.layouts.header')
+    <!-- Header End -->
+
+    @yield('content')
+
+    @include('frontend.layouts.footer')
+
+   
+
+   
+                        <div class="ft-social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-linkedin"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Js Plugins -->
+    <script src="frontend/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="frontend/assets/js/bootstrap.min.js"></script>
+    <script src="frontend/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="frontend/assets/js/jquery.countdown.min.js"></script>
+    <script src="frontend/assets/js/jquery.slicknav.js"></script>
+    <script src="frontend/assets/js/owl.carousel.min.js"></script>
+    <script src="frontend/assets/js/main.js"></script>
+</body>
+
+</html>
